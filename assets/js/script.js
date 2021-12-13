bc0120ec42a8dcaa2819420377040c463623a29a
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-12 18:00:53 -0600

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
 var formEL = document.querySelector("#task-form");
-var tasksToDoEL = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEL.appendChild(listItemEl);
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  // add html content to div 
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list (li) item to list (ul)
+  tasksToDoEl.appendChild(listItemEl);
+
+  console.dir(listItemEl);
 };
 
 // On a button click, create a task by running createTaskHandler function
 formEL.addEventListener("submit", createTaskHandler);
- 
